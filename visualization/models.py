e534444d73b78ccf17cae13a2e9b8da2f3f5aca8
spartonia <sd_prtn@yahoo.com>
2015-12-17 03:10:57 +0330

    OpenCellId model added

--- visualization/models.py
+++ visualization/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class OpenCellId(models.Model):
+    radio = models.CharField(
+        max_length=10
+    )
+    mcc = models.IntegerField()
+    net = models.IntegerField()
+    area = models.IntegerField()
+    cell = models.IntegerField()
+    unit = models.IntegerField(
+        null=True
+    )
+    lon = models.FloatField()
+    lat = models.FloatField()
+    range = models.IntegerField()
+    samples = models.IntegerField()
+    changeable = models.IntegerField()
+    created = models.IntegerField()
+    updated = models.IntegerField()
+    averageSignal = models.IntegerField()
