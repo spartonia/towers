5aed1afab8b24a3ca66e181cfce11d1b4874e33a
spartonia <sd_prtn@yahoo.com>
2016-03-25 19:38:41 +0100

    GAD1 added to show

--- visualization/views.py
+++ visualization/views.py
@@ -65,13 +65,13 @@ def index(request):
     #     b_line = [i[::-1] for i in b_line]
     #     towers.line(b_line, line_color='green', line_weight=5)
     #
-    # adm1_boundaries = []
-    # for adm1 in GADM1.objects.filter(name_0__iexact='Sweden'):
-    #     adm1_boundaries.append(adm1.geom.boundary)
-    # for boundary in adm1_boundaries:
-    #     for b_line in boundary:
-    #         b_line = [i[::-1] for i in b_line]
-    #     towers.line(b_line, line_color='blue', line_weight=1)
+    adm1_boundaries = []
+    for adm1 in GADM1.objects.filter(name_0__iexact='Sweden'):
+        adm1_boundaries.append(adm1.geom.boundary)
+    for boundary in adm1_boundaries:
+        for b_line in boundary:
+            b_line = [i[::-1] for i in b_line]
+        towers.line(b_line, line_color='blue', line_weight=1)
 
     for loc in coords:
         towers.circle_marker(
